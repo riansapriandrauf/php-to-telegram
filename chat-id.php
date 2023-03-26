0935c8068fd378cc31c2606481c279a66cb672f5
riansapriandrauf <riankwolo.57@gmail.com>
2023-03-26 16:47:53 +0800

    update

--- chat-id.php
+++ chat-id.php
@@ -1,10 +1,10 @@
 <?php
 // token bot Anda
-$token = " ";
+$token = "6117709271:AAGPDPczM9nAg0oquNo80zfIMQCKPACKjrs";
 // chat ID obrolan
-$chat_id = " ";
+$chat_id = "5923791304";
 // pesan yang ingin dikirim
-$message = "Hello, ini pesan dari PHP!";
+$message = "Hello, ini pesan dari Dari Boot Pengingat. <3";
 // URL untuk mengirim pesan
 $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=".urlencode($message);
 // kirim permintaan ke URL
